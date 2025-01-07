4dfd21e49e5f61204f4a519fab261678d37b10d2
StepanKhachikyan1999 <stepan-khachikyan@mail.ru>
2025-01-08 02:46:32 +0400

    added ctf

--- server.js
+++ server.js
@@ -7,10 +7,17 @@ import userRouter from "./Routes/UserRouter.js";
 import moviesRouter from "./Routes/MoviesRouter.js";
 import categoriesRouter from "./Routes/CategoriesRouter.js";
 import Uploadrouter from "./Controllers/UploadFile.js";
+import https from "https"
+import fs from "fs"
 
 dotenv.config();
 
 const app = express();
+
+const options = {
+  key: fs.readFileSync('~/certs/practice/server.key'), // replace it with your key path
+  cert: fs.readFileSync('~/certs/practice/server.crt'), // replace it with your certificate path
+}
 
 // Configure CORS
 const corsOptions = {
@@ -40,6 +47,13 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running at https://localhost:${PORT}`);
+https.createServer(options, (req, res) => {
+  res.writeHead(200);
+  res.end('Hello, HTTPS World!');
+}).listen(4000, () => {
+  console.log('Server is running on port 4000');
 });
+
+// app.listen(PORT, () => {
+//   console.log(`Server running at https://localhost:${PORT}`);
+// });
