79d148764ce5b81e5543d947cc81baf4f4565d0a
StepanKhachikyan1999 <stepan-khachikyan@mail.ru>
2025-01-08 02:25:17 +0400

    fix corse error

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at https://localhost:${PORT}`);
 });
